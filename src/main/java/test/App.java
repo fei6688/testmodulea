75fe6f21fa80b7b01e010657b09fb7153e78d599
fei6688 <15010087134@163.com>
2019-04-15 18:46:17 +0800

    初始提交

---
+++ src/main/java/test/App.java
@@ -0,0 +1,14 @@
+package main.java.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("哈哈哈");
+    }
+}
---
+++ src/test/java/test/AppTest.java
@@ -0,0 +1,18 @@
+package test.java.test;
+
+
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    public static void main(String[] args) {
+
+    }
+}
